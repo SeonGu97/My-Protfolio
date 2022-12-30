4df592c94e4acbd7940790f6d59bb8e2d4913b9a
강선구 <seongu97@naver.com>
2022-12-30 23:11:30 +0900

    포트폴리오 시안 - 전체적인 비율 수정(크게)

--- js/header/band/band.js
+++
@@ -1,7 +0,0 @@
-'ues strict';
-
-export default class Band {
-    constructor(creator, data, loop) {
-        loop(creator, data[2], data[1].name);
-    }
-}
--- js/header/logo/logo.js
+++
@@ -1,7 +0,0 @@
-'ues strict';
-
-export default class Logo {
-    constructor(creator, data, loop) {
-        loop(creator, data[3], data[1].name);
-    }
-}
